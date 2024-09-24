2a4347cf82cfae0833c6ad0590d033ab371c8bda
Ilonczai András <ilonczai.andras16@gmail.com>
2024-09-24 18:27:17 +0200

    UI Orders page without button

--- client/website/src/components/pages/orders/orders.js
+++ client/website/src/components/pages/orders/orders.js
@@ -1,9 +1,23 @@
-// src/pages/Home.js
 import React from 'react';
+import './orders.css';
 
 function Orders() {
   return (
-    <div>This is orders</div>
+    <div className='orders-container'>
+      <div className='header-for-children'>
+        <div className='header-item'>Feldolgozásra vár</div>
+        <div className='header-item'>Készül</div>
+        <div className='header-item'>Elkészült</div>
+      </div>
+      <div className='content-container'>
+        <div className='processing-container'></div>
+        <div className='about-container'></div>
+        <div className='ready-container'></div>
+      </div>
+      <div>
+        <button>+</button>
+      </div>
+    </div>
   );
 }
 
