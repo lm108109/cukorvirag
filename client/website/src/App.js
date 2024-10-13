c8c61551ae5cadb90b14e9c125edf71e090a95e9
Ilonczai András <ilonczai.andras16@gmail.com>
2024-10-13 18:55:02 +0200

    Refactor

--- client/website/src/App.js
+++ client/website/src/App.js
@@ -2,16 +2,16 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/header/header'
 import NavRoutes from './components/utils/navroutes/navroutes'
 import { AuthProvider } from './components/utils/AuthContext/AuthContext'
-import Main from './components/main/main'
+import MainContent from './components/main/main'
 import './App.css'
 
 function App() {
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col">
             <Router>
                 <AuthProvider>
                     <Header />
-                    <Main />
+                    <MainContent />
                 </AuthProvider>
             </Router>
         </div>
