3ee8dd069225323b01524336814b9938b7c8f447
Ilonczai András <ilonczai.andras16@gmail.com>
2024-12-07 13:41:59 +0100

    UI Login Logout Works

--- client/website/src/components/pages/log_out/log_out.js
+++ client/website/src/components/pages/log_out/log_out.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from 'react'
+import { useAuth } from '../../utils/AuthContext/AuthContext'
 
-function LogOut({ handleLogout }) {
-  return (
-    <div>
-      <h2>Are you sure you want to log out?</h2>
-      <button onClick={handleLogout}>Log Out</button>
-    </div>
-  );
+function LogOut() {
+    const { user, logoutUser } = useAuth()
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-[#f8f3ee] overflow-hidden">
+            <div className="w-[400px] p-[60px_40px] bg-[#ebd6d2] rounded-[15px] text-center">
+                <h2 className="text-[28px] font-light text-[#5e4e4c] mb-6">
+                    Biztosan ki szeretne jelentkezni?
+                </h2>
+                <div className="flex justify-around">
+                    <button
+                        onClick={logoutUser}
+                        className="w-[40%] p-[10px] bg-[#f9f5f2] border border-[#b2a09f] rounded-[30px] text-[18px] font-josefin-slab hover:bg-[#dfbbb0]"
+                    >
+                        Igen
+                    </button>
+                    <button
+                        onClick={() => window.history.back()} // Navigate back to the previous page
+                        className="w-[40%] p-[10px] bg-[#f9f5f2] border border-[#b2a09f] rounded-[30px] text-[18px] font-josefin-slab hover:bg-[#dfbbb0]"
+                    >
+                        Nem
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default LogOut;
+export default LogOut
